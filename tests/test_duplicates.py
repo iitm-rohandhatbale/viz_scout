b8a7c8bdcf2ec87d65a9fde5e4442279b624cac1
Rohan Dhatbale <rohandhatbale@Rohans-MacBook-Pro.local>
2024-12-27 12:53:50 +0530

    updated version and gitignore

--- tests/test_duplicates.py
+++ tests/test_duplicates.py
@@ -5,9 +5,6 @@ from viz_scout import DuplicateDetector
 def test_get_exact_duplicates():
     detector = DuplicateDetector(dataset_path="sample_datasets/coco5")
 
-    # Mocking `_generate_image_encoding` for simplicity
-    detector._generate_image_encoding = lambda: {"image1.png": "hash1", "image2.png": "hash1"}
-
     exact_duplicates = detector.get_exact_duplicates()
     near_duplicates = detector.get_near_duplicates()
     # assert duplicates == {"image1.png": ["image2.png"]}
